8cf58eb0550f5d9d8acc7cc68e6138dbfe3a038b
slloyd0621 <86664560+slloyd0621@users.noreply.github.com>
2021-06-29 17:25:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -137,7 +137,7 @@ resource "azurerm_virtual_machine" "catapp" {
 
   tags = {
     Department = "devops"
-    Billable = "true"
+    Billable = "tru"
     }
 
   # Added to allow destroy to work correctly.
